a552acde9d773451d213a6857a1f50fae6c14aea
46mht <46mht@LAPTOP-FGMFGFTF>
2023-09-04 11:02:51 +0300

    Proje dosyası ekle.

---
+++ WebUygulamaProje1/Utility/UygulamaDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebUygulamaProje1.Models;
+
+namespace WebUygulamaProje1.Utility
+{
+    public class UygulamaDbContext : DbContext
+    {
+        public UygulamaDbContext(DbContextOptions<UygulamaDbContext> options): base (options) { }
+        public DbSet<KitapTuru>KitapTurleri { get; set; }
+
+
+    }
+}
+
